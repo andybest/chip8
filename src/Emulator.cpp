752614c91906a8225e8c72988de5beea48d2fe35
Andy Best <andybest.net@gmail.com>
2016-09-07 17:41:09 +0100

    Added key handling, sprite rendering and run loop

--- src/Emulator.cpp
+++ src/Emulator.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <cstdlib>
 #include <ctime>
+#include <SDL.h>
 
 static const uint8_t CHIP8_FONT[] = {
     0xF0, 0x90, 0x90, 0x90, 0xF0,   // 0
@@ -21,9 +22,35 @@ static const uint8_t CHIP8_FONT[] = {
     0xF0, 0x80, 0xF0, 0x80, 0xF0,   // E
     0xF0, 0x80, 0xF0, 0x80, 0x80};  // F
 
+static const SDL_Keycode keyAssignments[] = {
+    SDLK_x,     // 0
+    SDLK_1,     // 1
+    SDLK_2,     // 2
+    SDLK_3,     // 3
+    SDLK_q,     // 4
+    SDLK_w,     // 5
+    SDLK_e,     // 6
+    SDLK_a,     // 7
+    SDLK_s,     // 8
+    SDLK_d,     // 9
+    SDLK_z,     // A
+    SDLK_c,     // B
+    SDLK_4,     // C
+    SDLK_r,     // D
+    SDLK_f,     // E
+    SDLK_v      // F
+};
+
+Emulator::Emulator()
+{
+    init();
+}
+
 
 void Emulator::init()
 {
+    quit = false;
+    waiting_for_key = false;
     srand(time(NULL));
     copy_font();
 }
@@ -32,6 +59,14 @@ void Emulator::copy_font()
 {
     // Copy the font into memory
     std::memcpy(memory, CHIP8_FONT, 5 * 16);
+}
+
+void Emulator::run_loop()
+{
+    while(!quit)
+    {
+        run_tick();
+    }
 }
 
 void Emulator::run_tick()
@@ -43,6 +78,50 @@ void Emulator::run_tick()
     if(soundTimer > 0) {
         --soundTimer;
     }
+
+    // Check events
+    SDL_Event e;
+    while(SDL_PollEvent(&e) != 0)
+    {
+        if(e.type == SDL_QUIT)
+            quit = true;
+        else if(e.type == SDL_KEYDOWN) {
+            int16_t key = check_keys(e.key.keysym.sym, true);
+            if(key > 0 && waiting_for_key)
+            {
+                waiting_for_key = false;
+                V[keyReg] = (uint8_t)key;
+            }
+        } else if(e.type == SDL_KEYUP) {
+            check_keys(e.key.keysym.sym, false);
+        }
+    }
+
+    if(!waiting_for_key)
+    {
+        for(int i = 0; i < 256; ++i)
+        {
+            uint16_t opcode = (memory[pc] << 8) | memory[pc + 1];
+            ++pc;
+            run_instruction(opcode);
+
+            if(waiting_for_key)
+                continue;
+        }
+    }
+}
+
+int16_t Emulator::check_keys(SDL_Keycode code, bool keyState)
+{
+    for(int i = 0; i < 0xF; ++i)
+    {
+        if(code == keyAssignments[i]) {
+            keys[i] = keyState;
+            return i;
+        }
+    }
+
+    return -1;
 }
 
 /*
@@ -160,7 +239,8 @@ void Emulator::run_instruction(uint16_t opcode)
    } else if(s == 0xF && kk == 0x07) {              // LD Vx, DT
        V[x] = delayTimer;
    } else if(s == 0xF && kk == 0x0A) {              // LD Vx, K
-       V[x] = wait_key_press();
+       keyReg = x;
+       wait_key_press();
    } else if(s == 0xF && kk == 0x15) {              // LD DT, Vx
        delayTimer = V[x];
    } else if(s == 0xF && kk == 0x18) {              // LD ST, Vx
@@ -205,11 +285,35 @@ uint16_t Emulator::pop_stack()
     return rv;
 }
 
-void Emulator::display_sprite(uint16_t address, uint8_t x, uint8_t y) {
+void Emulator::display_sprite(uint16_t num_bytes, uint8_t sx, uint8_t sy)
+{
+    V[0xF] = 0;
 
+    for(int y = 0; y < num_bytes; y++)
+    {
+        int idx = (I + y) % 4096;
+        uint8_t sprite = memory[idx];
+        uint16_t bufY = (y + sy) % 32;
+
+        for(int x = 0; x < 8; x++)
+        {
+            uint8_t bit = (sprite >> (7 - x)) & 0x1;
+            uint16_t bufX = (x + sx) % 64;
+
+            uint16_t screenIdx = bufX + (bufY * 64);
+            uint8_t oldVal = screenBuf[screenIdx];
+            screenBuf[screenIdx] ^= bit;
+            if(oldVal == 1 && screenBuf[screenIdx] == 0)
+                V[0xF] = 1;
+        }
+    }
 }
 
-uint8_t Emulator::wait_key_press()
+void Emulator::wait_key_press()
 {
-    return 0;
+    if(!waiting_for_key)
+    {
+        waiting_for_key = true;
+        return;
+    }
 }
--- src/Emulator.h
+++ src/Emulator.h
@@ -1,10 +1,17 @@
 #pragma once
 
 #include <stdint.h>
+#include <SDL.h>
 
 class Emulator
 {
+    public:
+        Emulator();        
+        void run_loop();
+
     private:
+        bool quit;
+
         uint8_t memory[4096];
         uint8_t V[16];                  // Registers
         uint16_t I;                     // Index register
@@ -14,6 +21,8 @@ class Emulator
         uint16_t sp;                    // Stack pointer
 
         bool keys[16];                  // Key array
+        uint8_t keyReg;                 // Register to write key to
+        bool waiting_for_key;
 
         static const uint16_t screenBufSize = 64 * 32;
         uint8_t screenBuf[screenBufSize];     // Screen buffer
@@ -26,10 +35,11 @@ class Emulator
         void copy_font();
         void run_tick();
         void run_instruction(uint16_t opcode);
+        int16_t check_keys(SDL_Keycode code, bool keyState);
 
         void push_stack(uint16_t value);
         uint16_t pop_stack();
 
-        void display_sprite(uint16_t address, uint8_t x, uint8_t y);
-        uint8_t wait_key_press();
+        void display_sprite(uint16_t num_bytes, uint8_t x, uint8_t y);
+        void wait_key_press();
 };
